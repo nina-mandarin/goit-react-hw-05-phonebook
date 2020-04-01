eeb0cbcaad244a945a63afad6466ab0af422df58
Nina <62608693+Nina-freysoft@users.noreply.github.com>
2020-04-01 23:44:06 +0300

    Updates

---
+++ precache-manifest.55b8de95fb0cd3384bf421fe873a653f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95ad62561c134f11194b2af3cf4f53d",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "e7363a3e15bc56401059",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.5db1fdcf.chunk.css"
+  },
+  {
+    "revision": "8ab67912baa49c96aae8",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.c60261cf.chunk.js"
+  },
+  {
+    "revision": "b315737083144f4c14b16a4a43df1485",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.c60261cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7363a3e15bc56401059",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.48deaf89.chunk.js"
+  },
+  {
+    "revision": "c30e26e828e21fa05513",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.78e82c93.js"
+  }
+]);
